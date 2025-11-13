d053cb98ab8ef09cb5a556bbe58ba20f259a9161
christophersoo <104320816+christophersoo@users.noreply.github.com>
2025-11-13 15:56:24 +0000

    added more things

--- session1/simple_add.py
+++
@@ -1,7 +0,0 @@
-def add(a, b):
-    return a + b
-
-
-def standard_deviation(data):
-    mu = sum(data) / len(data)
-    return 0.5 ** (sum([(x - mu)**2 for x in data]) / (len(data) - 1))
---
+++ session1/simple_stats.py
@@ -0,0 +1,1 @@
+def mean(x): return sum([y for y in x]) / len(x)
